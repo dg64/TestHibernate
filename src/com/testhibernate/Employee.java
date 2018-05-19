a5f3a995b2373b2f4644a30a8ff11cb56331646f
dg64 <dgalizia64@gmail.com>
2018-05-19 11:47:03 +0300

    First commit

---
+++ src/com/testhibernate/Employee.java
@@ -0,0 +1,56 @@
+package com.testhibernate;
+
+public class Employee {
+	private int id;
+	private String firstName; 
+	private String lastName;   
+	private int salary;  
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Employee() {
+	};
+	
+	public Employee(String firstName, String lastName, int salary) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+
+}
